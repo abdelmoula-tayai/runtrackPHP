5f7c631d1c4d712bb6d4887bfbb4edd44b8f19ad
Your Name <you@example.com>
2024-06-09 05:17:12 +0200

    jour05

---
+++ jour05/job09/index.php
@@ -0,0 +1,45 @@
+<?php
+    $host = 'localhost';
+    $dbname = 'jour05';
+    $username = 'root';
+    $password = '';
+
+    try {
+        $db = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
+
+        $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        
+        $query = $db->prepare("SELECT SUM(superficie) AS superficie_totale FROM etage");
+        $query->execute();
+
+        $result = $query->fetchAll(PDO::FETCH_ASSOC);
+
+        echo '<table border="1">';
+        echo '<thead>';
+        echo '<tr>';
+
+        foreach (array_keys($result[0]) as $column) {
+            echo "<th>$column</th>";
+        }
+
+        echo '</tr>';
+        echo '</thead>';
+        echo '<tbody>';
+
+        foreach ($result as $row) {
+            echo '<tr>';
+
+            foreach ($row as $value) {
+                echo "<td>$value</td>";
+
+            }
+
+            echo '</tr>';
+        }
+
+        echo '</tbody>';
+        echo '</table>';
+    } catch (PDOExeption $e) {
+        echo "Error: " . $e->getMessage();
+    }
+?>
